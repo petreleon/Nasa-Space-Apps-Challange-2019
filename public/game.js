7e589f95956bc385bd5be1ceb2fe0326fb53fce4
Petre Leonard Macamete <petreleonardos@gmail.com>
2019-10-20 12:57:45 +0300

    commit

--- public/game.js
+++ public/game.js
@@ -1,49 +1,114 @@
 location_ = 'assets/deseuri/';
-
+// metal- paper- glass- organic- refuse- plastic- tetrapack-
 const assets = {
-  aluminum: 20,
-  coffeecup: 6,
-  glass: 18,
-  organics: 23,
-  paper: 17,
-  plastic: 15,
-  plasticbag: 1,
-  refuse: 32,
-  ruinedpaper: 20,
-  styrofoam: 5,
-  tetrapack: 11,
-  tin: 19
+  aluminum: 20, // metal
+  coffeecup: 6, // paper
+  glass: 18, // glass
+  organics: 23, //organic
+  paper: 17, //paper
+  plastic: 15, // plastic
+  plasticbag: 1, // plastic
+  refuse: 32, // refuse
+  ruinedpaper: 20, //refuse 
+  styrofoam: 5, // refuse
+  tetrapack: 11, // tetrapack 
+  tin: 19 //metal
 };
 randomTextures = [];
+
+
 
 textures = {};
 
 for (material in assets) {
-  textures[material]=[];
+  textures[material] = [];
   for (i = 1; i <= assets[material]; i++) {
     textures[material].push(PIXI.Texture.from(location_ + material + i + '.svg'));
     randomTextures.push(PIXI.Texture.from(location_ + material + i + '.svg'));
   }
 }
 
-const app = new PIXI.Application({ backgroundColor: 0x1099bb });
+categories = Object.entries(assets);
+
+const app = new PIXI.Application({ backgroundColor: 0x113d4c, height:800 });
 document.body.appendChild(app.view);
 
+const graphics = new PIXI.Graphics();
+
+graphics.beginFill(0xDE3249);
+graphics.drawRect(600, 50, 200, 100);
+graphics.endFill();
+
+const refuseText = new PIXI.Text('refuse');
+refuseText.x = 620;
+refuseText.y = 100;
+
+const paperText = new PIXI.Text('organic');
+paperText.x = 620;
+paperText.y = 200;
+
+const plasticText = new PIXI.Text('plastic');
+plasticText.x = 620;
+plasticText.y = 300;
+
+const organicText = new PIXI.Text('organic');
+organicText.x = 620;
+organicText.y = 400;
+
+const glassText = new PIXI.Text('glass');
+glassText.x = 620;
+glassText.y = 500;
+
+const tetrapackText = new PIXI.Text('tetrapack');
+tetrapackText.x = 620;
+tetrapackText.y = 600;
+
+const metalText = new PIXI.Text('metal');
+metalText.x = 620;
+metalText.y = 700;
+
+graphics.beginFill(0x20fc03);
+graphics.drawRect(600, 150, 200, 100);
+graphics.endFill();
+
+graphics.beginFill(0xDE3249);
+graphics.drawRect(600, 250, 200, 100);
+graphics.endFill();
+
+graphics.beginFill(0x20fc03);
+graphics.drawRect(600, 350, 200, 100);
+graphics.endFill();
+
+graphics.beginFill(0xDE3249);
+graphics.drawRect(600, 450, 200, 100);
+graphics.endFill();
+
+graphics.beginFill(0x20fc03);
+graphics.drawRect(600, 550, 200, 100);
+graphics.endFill();
+
+graphics.beginFill(0xDE3249);
+graphics.drawRect(600, 650, 200, 100);
+graphics.endFill();
 
 // Scale mode for pixelation
 arr = [];
 Dragging = false;
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 30; i++) {
   arr.push(createBunny(
-    Math.floor(Math.random() * (app.screen.width-200)),
+    Math.floor(Math.random() * (app.screen.width - 200)),
     Math.floor(Math.random() * app.screen.height),
   ));
 }
 
 function createBunny(x, y) {
   // create our little bunny friend..
-  const bunny = new PIXI.Sprite(randomTextures[Math.floor(Math.random() * randomTextures.length)]);
+  var category = categories[Math.floor(Math.random() * categories.length)][0];
 
+  var texturesOfCategory = textures[category];
+  const bunny = new PIXI.Sprite(texturesOfCategory[Math.floor(Math.random() * texturesOfCategory.length)]);
+  bunny.category = category;
+  //bunny.category = categoriy;
   // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
   bunny.interactive = true;
 
@@ -85,6 +150,16 @@ function createBunny(x, y) {
   return bunny;
 }
 
+app.stage.addChild(graphics);
+
+app.stage.addChild(refuseText);
+app.stage.addChild(paperText);
+app.stage.addChild(plasticText);
+app.stage.addChild(organicText);
+app.stage.addChild(glassText);
+app.stage.addChild(tetrapackText);
+app.stage.addChild(metalText);
+
 function onDragStart(event) {
   // store a reference to the data
   // the reason for this is because of multitouch
@@ -102,6 +177,53 @@ function onDragEnd() {
   // set the interaction data to null
   this.data = null;
   Dragging = false;
+  //const newPosition = this.data.getLocalPosition(this.parent);
+  //refuse
+  if (this.x >= 600 && this.x < 800 && this.y >= 50 && this.y < 150) {
+    if (['refuse', 'styrofoam', 'ruinedpaper'].includes(this.category)) console.log('success');
+    else console.log('fall')
+    this.x = 900;
+  }
+  //paper
+  if (this.x >= 600 && this.x < 800 && this.y >= 150 && this.y < 250) {
+    if (
+    ['paper', 'coffecup'].includes(this.category)
+    ) console.log('success');
+    else console.log('fall')
+    this.x = 900;
+  }
+  //plastic
+  if (this.x >= 600 && this.x < 800 && this.y >= 250 && this.y < 350) {
+    if (['plastic', 'plasticbag'].includes(this.category)
+      ) console.log('success');
+    else console.log('fall')
+    this.x = 900;
+  }
+  //organics
+  if (this.x >= 600 && this.x < 800 && this.y >= 350 && this.y < 450) {
+    if (['organics'].includes(this.category)) console.log('success');
+    else console.log('fall')
+    this.x = 900;
+  }
+  //glass
+  if (this.x >= 600 && this.x < 800 && this.y >= 450 && this.y < 550) {
+    if (['glass'].includes(this.category)) console.log('success');
+    else console.log('fall')
+    this.x = 900;
+  }
+  //tetrapack
+  if (this.x >= 600 && this.x < 800 && this.y >= 550 && this.y < 650) {
+    if (['tetrapack'].includes(this.category)) console.log('success');
+    else console.log('fall')
+    this.x = 900;
+  }
+  //metal
+  if (this.x >= 600 && this.x < 800 && this.y >= 650 && this.y < 750) {
+    if (['aluminum', 'tin'].includes(this.category)) console.log('success');
+    else console.log('fall')
+    this.x = 900;
+  }
+  //console.log(graphics.containsPoint(newPosition.x, newPosition.y));
 }
 
 function onDragMove() {
